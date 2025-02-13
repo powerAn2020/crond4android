cceeb22910e81310e2c66f1dac24b0fba5228108
dawn-lc <30336566+dawn-lc@users.noreply.github.com>
2025-02-13 10:13:04 +0800

    更新 customize.sh

--- customize.sh
+++ customize.sh
@@ -13,19 +13,19 @@ if [ ! -d "${cronDataDir}" ]; then
 fi
 
 ui_print "- Installing crontab command"
-mkdir -p "${MODPATH}/system/xbin" && cd "${MODPATH}/system/xbin"
+mkdir -p "${MODPATH}/system/xbin"
 {
   echo "#!/system/bin/sh"
   if [ "$KSU" = true ]; then
-    echo "/data/adb/ksu/bin/busybox crontab -c '${cronDataDir}' \"\$@\""
+    echo "/data/adb/ksu/bin/busybox crontab -c '${cronDataDir}' $@"
   elif [ "$APATCH" = true ]; then
-    echo "/data/adb/ap/bin/busybox crontab -c '${cronDataDir}' \"\$@\""
+    echo "/data/adb/ap/bin/busybox crontab -c '${cronDataDir}' $@"
   else
-    echo "/data/adb/magisk/busybox crontab -c '${cronDataDir}' \"\$@\""
+    echo "/data/adb/magisk/busybox crontab -c '${cronDataDir}' $@"
   fi
-} > crontab
-set_perm crontab 0 0 0755
+} > "${MODPATH}/system/xbin/crontab"
 
 ui_print "- Setting permissions"
+set_perm "${MODPATH}/system/xbin/crontab" 0 0 0755
 set_perm "${MODPATH}/service.sh" 0 0 0755
 set_perm "${MODPATH}/uninstall.sh" 0 0 0755
